6431f593af03d05608fe6002a86341b9b22fecf9
Harish Karthick S <harishskarthick.s@gmail.com>
2026-09-05 19:52:04 +0530

    Seed Harbor Bindery invoices, papers, and payments

--- sql_playground/data/__init__.py
+++ sql_playground/data/__init__.py
@@ -2,11 +2,13 @@ from sql_playground.data.connection import Database, default_paths
 from sql_playground.data.executor import run_sql
 from sql_playground.data.saved_queries import SavedQueryStore
 from sql_playground.data.schema import inspect_schema
+from sql_playground.data.seed import ensure_seeded
 
 __all__ = [
     "Database",
     "SavedQueryStore",
     "default_paths",
+    "ensure_seeded",
     "inspect_schema",
     "run_sql",
 ]
